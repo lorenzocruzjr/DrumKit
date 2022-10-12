d4f2cb13716892f5585c787717cb6d6efe55e984
lorenzocruzjr <lorenzocruzjr1988@gmail.com>
2022-10-12 15:24:21 +0800

    removed extra indention in html

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for(var i = 0; i<numberOfDrumButtons; i++){
 
   document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-  
+
     var buttonInnerHTML = this.innerHTML;
 
     makeSound(buttonInnerHTML);
 
     buttonAnimation(buttonInnerHTML);
-  
+
     console.log(this.innerHTML);
 
   });
@@ -45,7 +45,7 @@ function makeSound(key){
       var tom1 = new Audio('sounds/tom-1.mp3');
       tom1.play();
       break;
-      
+
     case "a":
       var tom2 = new Audio('sounds/tom-2.mp3');
       tom2.play();
@@ -60,7 +60,7 @@ function makeSound(key){
       var tom4 = new Audio('sounds/tom-4.mp3');
       tom4.play();
       break;
-    
+
     case "j":
       var crash = new Audio('sounds/crash.mp3');
       crash.play();
@@ -85,7 +85,7 @@ function makeSound(key){
 function buttonAnimation(currentKey){
 
   var activeButton = document.querySelector("." + currentKey);
-  
+
   activeButton.classList.add("pressed");
 
   setTimeout(function(){
